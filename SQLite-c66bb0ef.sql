b2ddeddd1b6997c5772e1af4cf68a38789246a61
Mohamed <98917054+MohaZ0@users.noreply.github.com>
2026-04-20 13:13:47 +0000

    Ajout export SQL (SQLite-c66bb0ef.sql)

---
+++ SQLite-c66bb0ef.sql
@@ -0,0 +1,29 @@
+ 
+ --Requete de base 
+ 
+ /*SELECT * 
+ FROM ventes;
+*/
+
+
+
+--Chiffre d'affaires total
+
+/*SELECT SUM(prix * qte)
+FROM ventes;
+*/
+
+
+
+--Ventes par produit 
+
+/*SELECT produit, SUM(qte) AS quantite
+FROM ventes
+GROUP BY produit;
+*/
+
+--Ventes par region
+
+SELECT region, SUM(qte) AS Quantite
+FROM ventes
+GROUP BY region;
